b612be7aff447cbcc640aa8e060b53c5ecfee2d0
tanishijanweja <tanishi.janweja@gmail.com>
2025-06-27 13:42:13 +0530

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+import { Suspense } from "react";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { Client } from "./Client";
 
 const Page = async () => {
-  return <div>Hello</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "Tanishi PREFETCH" })
+  );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
